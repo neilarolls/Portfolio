7a32d9678c5c5cbb8121a0e0c3de1b8bcd786f2c
neilarolls <neilarolls@gmail.com>
2026-04-13 09:40:30 +0100

    Working on slide-out menu

--- js/bm-js.js
+++ js/bm-js.js
@@ -4,19 +4,20 @@ let menu = document.querySelector("#bm-display");                               
 
 function updateMenuDisplay() {                                                            // Equivalent to @media call to display menu at 576px or greater width
   if (window.innerWidth >= 576) {
-    menu.style.display = 'block';
+    // menu.style.display = 'block';
   } else {
-    menu.style.display = 'none';
+    // menu.style.display = 'none';
   }
 }
 
-if (button && menu) {                                                     // Good practice is to check that variables exist before using them to avoid errors.
+if (button && menu) {                                                     // Good practice is to check that things exist before using them to avoid errors.
   button.addEventListener("click", function() {                           // Attaches event listener to button listening for a click event.
-    const isHidden = window.getComputedStyle(menu).display === "none";    // sets isHidden to true/false depending on menu visibility
-    menu.style.display = isHidden ? "block" : "none";                     // toggles the menu display value dependent on value of isHidden
+    // const isHidden = window.getComputedStyle(menu).display === "none";    // sets isHidden to true/false depending on menu visibility
+                                                                          // 
+    // menu.style.display = isHidden ? "block" : "none";                     // toggles the menu display value dependent on value of isHidden
     sound.play();                                                         // plays 'click' audio
   });
 }
 
-window.addEventListener('resize', updateMenuDisplay);                     // adds event listener to watch for window resizing, triggering a media width check and conditional hide/show of menu.
-updateMenuDisplay();                                                      // checks when first loaded or the page could be displayed incorrectly
+window.addEventListener('resize', updateMenuDisplay);                     // Adds event listener to watch for window resizing, triggering a media width check and conditional hide/show of menu.
+updateMenuDisplay();                                                      // Checks when first loaded or the page could be displayed incorrectly
