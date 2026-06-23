0966e11959f2530ae32230573ecadac86c5b3bb0
neilarolls <neilarolls@gmail.com>
2026-06-23 12:01:12 +0100

    Fixed coding examples.
    Now back to a consistent display.
    Responsiveness is sorted.
    Need to add selection controls to the display.

--- js/show-code.js
+++ js/show-code.js
@@ -1,32 +1,7 @@
 window.onload = (e) => {
 
     const titleWrapper = document.getElementById("code-transporter-header");
-    const animWrapper = document.getElementById("code-transporter-content");
-    const topWrapper = document.getElementById("coding-top");
-    const wrapperHeight = (animWrapper.clientHeight);
-
-    console.log(`header          ${titleWrapper}`);
-    console.log(`wrapper         ${animWrapper}`);
-    console.log(`wrapper height  ${wrapperHeight}`);
-
-    let targetY = titleWrapper.offsetHeight;
-
-    console.log(`targetY   ${targetY}`);
-
-    // const block1 = document.getElementById("code-anim-block-1");
-    // const block2 = document.getElementById("code-anim-block-2");
-    // const block3 = document.getElementById("code-anim-block-3");
-    // const block4 = document.getElementById("code-anim-block-4");
-    // const block5 = document.getElementById("code-anim-block-5");
-    // const block6 = document.getElementById("code-anim-block-6");
-    // const block7 = document.getElementById("code-anim-block-7");
-    // const block8 = document.getElementById("code-anim-block-8");
-    // const block9 = document.getElementById("code-anim-block-9");
-    // const block10 = document.getElementById("code-anim-block-10");
-
-
-    $(animWrapper).css({"top":`${targetY - wrapperHeight - 500}px`});
-    
-    $(animWrapper).animate({"top":`${targetY + 125}`}, 1250);
+    const contentWrapper = document.getElementById("code-transporter-content");
+    const contentWrapperHeight = (contentWrapper.offsetHeight);
 
 };
