e956e942d8d36a473b71b11f781390cbe60aaad1
neilarolls <neilarolls@gmail.com>
2026-06-25 07:49:48 +0100

    Update
    Have a hidden copy of the first code example in place ready for new data. The id's and classes are all set up. Then it will be some js for controls to switch between them.

--- js/show-code.js
+++ js/show-code.js
@@ -2,6 +2,5 @@ window.onload = (e) => {
 
     const titleWrapper = document.getElementById("code-transporter-header");
     const contentWrapper = document.getElementById("code-transporter-content");
-    const contentWrapperHeight = (contentWrapper.offsetHeight);
 
 };
